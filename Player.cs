5a0882a408c9804d395d7d39c773e7187e61f8e5
marshalb <marshalb@mail.gvsu.edu>
2016-09-17 16:29:31 -0400

    Add files via upload
    Script that moves an object based on arrow-key presses.

---
+++ Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    float speed = 2.5f;
+    float test = 2;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    //Player movement, taken currently from arrow keys
+    //May change to wsad later
+    void Move()
+    {
+        if (Input.GetKey("up"))
+            transform.Translate(0, speed * Time.deltaTime, 0);
+        if (Input.GetKey("down"))
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+        if (Input.GetKey("left"))
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        if (Input.GetKey("right"))
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+
+
+}
