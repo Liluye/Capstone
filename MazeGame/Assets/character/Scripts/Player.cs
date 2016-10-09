552779c8ded90df57755bc964026835fe27d7a91
algermo <algermo@mail.gvsu.edu>
2016-10-09 13:12:34 -0400

    fixed animation transitions

--- MazeGame/Assets/character/Scripts/Player.cs
+++ MazeGame/Assets/character/Scripts/Player.cs
@@ -85,7 +85,7 @@ public class Player : MonoBehaviour
             } else if (currentAnimationState == STATE_WALKR)
             {
                 changeState(STATE_IDLER);
-            } else 
+            } else if (currentAnimationState == STATE_WALKL)
             {
                 changeState(STATE_IDLEL);
             }
