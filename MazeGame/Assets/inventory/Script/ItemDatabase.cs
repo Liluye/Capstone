e237b77bcfd8feef1d62ede211b6685b36c430da
Mick Lautenbach <micklautenbach1@gmail.com>
2016-10-25 17:39:18 -0400

    Created script for inventory, added LitJson.dll for json library

---
+++ MazeGame/Assets/inventory/Script/ItemDatabase.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using LitJson;
+using System.Collections.Generic;
+using System.IO;
+
+public class InventoryDatabase : MonoBehaviour {
+	private List<Item> database = new List<Item>();
+	private JsonData itemData;
+
+	void Start()
+	{
+		itemData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/inventory/StreamingAssets/Items.Json"));
+	}
+}
+
+public class Item
+{
+
+}
