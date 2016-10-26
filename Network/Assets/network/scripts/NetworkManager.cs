cda632668b0c78e6e96e4a0ecdae83778e9be78f
April Lautenbach <april.lautenbach@gmail.com>
2016-10-26 14:51:13 -0400

    Networking project
    This is the Unity project I'm using to implement and test networking
    scripts so I don't overlap my work with anyone else.

---
+++ Network/Assets/network/scripts/NetworkManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//First get access to the DarkRift namespace
+using DarkRift;
+
+public class NetworkManager : MonoBehaviour
+{
+	//Using local network for now.
+	public string IP = "127.0.0.1";
+
+	void Start()
+	{
+		DarkRiftAPI.Connect(IP);
+	}
+
+	//For now disconnect on app close, but want it to disconnect after data transfer.
+	void OnApplicationQuit()
+	{
+		DarkRiftAPI.Disconnect();
+	}
+}
