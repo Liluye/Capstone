644ed933381cf14188ae161f68c1417622709c22
Mick Lautenbach <micklautenbach1@gmail.com>
2016-10-26 14:54:12 -0400

    Worked on inventory script

--- MazeGame/Assets/inventory/Script/ItemDatabase.cs
+++ MazeGame/Assets/inventory/Script/ItemDatabase.cs
@@ -4,17 +4,49 @@ using LitJson;
 using System.Collections.Generic;
 using System.IO;
 
-public class InventoryDatabase : MonoBehaviour {
+public class ItemDatabase : MonoBehaviour {
 	private List<Item> database = new List<Item>();
 	private JsonData itemData;
 
 	void Start()
 	{
 		itemData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/inventory/StreamingAssets/Items.Json"));
+		ConstructItemDatabase ();
+		Debug.Log (FetchItemById(0).Title);
+
+	}
+
+	public Item FetchItemById(int id)
+	{
+		for (int i = 0; i < database.Count; i++) 
+			if (database [i].ID == id)
+				return database [i];
+		return null;
+	}
+
+	void ConstructItemDatabase()
+	{
+		for (int i = 0; i < itemData.Count; i++) 
+		{
+			database.Add (new Item ((int)itemData[i]["id"], itemData[i]["title"].ToString(), itemData[i]["description"].ToString(), 
+				(bool)itemData[i]["stackable"]));
+		}
 	}
 }
 
 public class Item
 {
+	public int ID { get; set; }
+	public string Title { get; set; }
+	public string Description { get; set; }
+	public bool Stackable { get; set; }
 
+
+	public Item(int id, string title, string description, bool stackable)
+	{
+		this.ID = id;
+		this.Title = title;
+		this.Description = description;
+		this.Stackable = stackable;
+	}
 }
