a0f5d18b4102eb884c65fd40cb52c520cff87db3
Scott Wilson <wilsonsc@mail.gvsu.edu>
2016-10-29 12:29:45 -0400

    Boomerang animation

---
+++ Items/Assets/enemy/Enemy.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+
+    Animator animator;
+
+    //travel 2.5 grid squares per second
+    float speed = 2.5f;
+    // character animations states
+    // note: states added within unity and tied to animation
+    const int STATE_IDLED = 0;
+    const int STATE_IDLEU = 1;
+    const int STATE_IDLER = 2;
+    const int STATE_IDLEL = 3;
+    const int STATE_WALKD = 4;
+    const int STATE_WALKU = 5;
+    const int STATE_WALKR = 6;
+    const int STATE_WALKL = 7;
+
+    // character starts out in idle state
+    int currentAnimationState = STATE_IDLED;
+
+    // Use this for initialization
+    void Start()
+    {
+        // define animator attached to character
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    // note: FixedUpdate instead of Update keeps sprite from jittering on collisions
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+
+        if (Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d"))
+        {
+            System.Random rnd = new System.Random();
+            int randomMove = rnd.Next(0, 4);
+            if(randomMove == 0)
+            {
+                changeState(STATE_WALKU);
+                transform.Translate(0, speed * Time.deltaTime, 0);
+                changeState(STATE_IDLEU);
+            } else if(randomMove == 1)
+            {
+                changeState(STATE_WALKD);
+                transform.Translate(0, -speed * Time.deltaTime, 0);
+                changeState(STATE_IDLED);
+            } else if(randomMove == 2)
+            {
+                changeState(STATE_WALKL);
+                transform.Translate(-speed * Time.deltaTime, 0, 0);
+                changeState(STATE_IDLEL);
+            } else if(randomMove == 3)
+            {
+                changeState(STATE_WALKR);
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+                changeState(STATE_IDLER);
+            }
+           
+        }
+
+    }
+
+    void changeState(int state)
+    {
+        // note: Has Exit Time must not be checked or animation will not loop
+        if (currentAnimationState == state)
+            return;
+
+        switch (state)
+        {
+            case STATE_IDLED:
+                animator.SetInteger("state", STATE_IDLED);
+                break;
+            case STATE_IDLEU:
+                animator.SetInteger("state", STATE_IDLEU);
+                break;
+            case STATE_IDLER:
+                animator.SetInteger("state", STATE_IDLER);
+                break;
+            case STATE_IDLEL:
+                animator.SetInteger("state", STATE_IDLEL);
+                break;
+            case STATE_WALKD:
+                animator.SetInteger("state", STATE_WALKD);
+                break;
+            case STATE_WALKU:
+                animator.SetInteger("state", STATE_WALKU);
+                break;
+            case STATE_WALKR:
+                animator.SetInteger("state", STATE_WALKR);
+                break;
+            case STATE_WALKL:
+                animator.SetInteger("state", STATE_WALKL);
+                break;
+        }
+        currentAnimationState = state;
+    }
+}
---
+++ Items/Assets/items/scripts/boomerangAction.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boomerangAction : MonoBehaviour {
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private Vector3 startLoc = new Vector3(0, 0, 0);
+    private Vector3 destinationLoc;
+    public Sprite[] sprites;
+    public float framesPerSecond;
+    public float speed;
+    private bool returning = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        spriteRenderer = GetComponent<Renderer>() as SpriteRenderer;
+        this.startLoc = transform.position;
+        setInitialDestination(2);
+    }
+
+    void Start(Vector3 startLoc, int direction)
+    {
+        this.Start();
+        this.startLoc = startLoc;
+        setInitialDestination(direction);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentPosition = transform.position;
+
+        if (Vector3.SqrMagnitude(currentPosition - destinationLoc) < 0.2)
+        {
+            if (returning)
+                Destroy(gameObject);
+            else
+                boomerangReturn();
+        }
+
+        int nextSprite = (int)(Time.timeSinceLevelLoad * framesPerSecond);
+        nextSprite %= sprites.Length;
+        spriteRenderer.sprite = sprites[nextSprite];
+
+        Vector3 target = destinationLoc;
+
+        transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime);
+        //Debug.Log(Time.deltaTime);
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        boomerangReturn();
+    }
+
+    private void setInitialDestination(int direction)
+    {
+        destinationLoc.x = startLoc.x;
+        destinationLoc.y = startLoc.y;
+        //0 down, 1 left, 2 up, 3 right
+        switch (direction)
+        {
+            case 0:
+                destinationLoc.y = startLoc.y - 4;
+                break;
+            case 1:
+                destinationLoc.x = startLoc.x - 4;
+                break;
+            case 2:
+                destinationLoc.y = startLoc.y + 4;
+                break;
+            case 3:
+                destinationLoc.x = startLoc.x + 4;
+                break;
+            default:
+                //something went wrong, do nothing!
+                break;
+        }
+    }
+
+    private void boomerangReturn()
+    {
+        destinationLoc = startLoc;
+        returning = true;
+    }
+}
