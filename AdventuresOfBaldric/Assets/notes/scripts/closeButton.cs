2d56398e12a57727c6042e4ab52e816784958de7
algermo <algermo@mail.gvsu.edu>
2016-12-04 20:44:01 -0500

    finished commenting scripts

--- AdventuresOfBaldric/Assets/notes/scripts/closeButton.cs
+++ AdventuresOfBaldric/Assets/notes/scripts/closeButton.cs
@@ -1,25 +1,43 @@
-﻿using UnityEngine;
+﻿/*****************************************************************
+Script to determine what the close button does on the note.
+
+@author The Adventures of Baldric
+@version Fall 2016
+*****************************************************************/
+
+using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
 public class closeButton : MonoBehaviour {
 
-	public Button button;
+    /** close button */
+    public Button button;
 
-	//add click listener to button
-	void Start () {
-		Button btn = button.GetComponent<Button>();
+    /*******************************************************************
+	 * Method used for initialization
+	 ******************************************************************/
+    void Start () {
+
+        // add click listener to button
+        Button btn = button.GetComponent<Button>();
 		btn.onClick.AddListener(TaskOnClick);
+
 	}
 
-	//when button is clicked close panel prefab
-	void TaskOnClick(){
-		if (GameObject.Find ("writeNotePanel(Clone)") != null) {
+    /*******************************************************************
+	 * Method to determine what to do on click
+	 ******************************************************************/
+    void TaskOnClick(){
+
+        // when button is clicked close panel prefab
+        if (GameObject.Find ("writeNotePanel(Clone)") != null) {
 			Destroy (GameObject.Find ("writeNotePanel(Clone)"));
 		} else {
 			Destroy (GameObject.Find ("readNotePanel(Clone)"));
 		}
-		//unpause game action
+
+		// unpause game action
 		Time.timeScale = 1.0f;
 	}
 }
